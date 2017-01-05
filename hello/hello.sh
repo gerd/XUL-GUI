cf496e8049b876d614be59aa96e7b6ed324511d7
Gerd Pokorra <gz016@hilton.zimt.uni-siegen.de>
2017-01-05 21:37:23 +0100

    make the invoke independent

--- hello/hello.sh
+++ hello/hello.sh
@@ -1,1 +1,7 @@
-firefox -app /home/gz016/hello/application.ini -no-remote
+#!/bin/sh
+
+# make the invoke of the program working independent from the syntax and place
+# of the sources
+cd `dirname $0`
+
+firefox -app `pwd`/application.ini -no-remote
