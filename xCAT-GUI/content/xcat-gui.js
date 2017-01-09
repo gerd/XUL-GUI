ca0b3301db48b02964fc5d8b69ad8e617a36ade8
gerd <gz016@maritim.zimt.uni-siegen.de>
2017-01-09 11:10:33 +0100

    separate javascript code to separate file

---
+++ xCAT-GUI/content/xcat-gui.js
@@ -0,0 +1,8 @@
+function onOK() {
+    var text = document.getElementById("username").value;
+    alert(document.getElementById("host").value + ':' +
+        text + ':' +
+        document.getElementById("password").value);
+
+    // add first https get request with javascript
+}
